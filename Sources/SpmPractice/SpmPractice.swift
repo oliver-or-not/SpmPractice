e252ca0658360ce790a224dd7ae6a712f77058c9
socar-taro <taro@socar.kr>
2024-03-28 10:58:46 +0900

    protocol extension.

--- Sources/SpmPractice/SpmPractice.swift
+++ Sources/SpmPractice/SpmPractice.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public protocol PPP {
+    var x: Int { get set }
+}
+
+extension PPP {
+    func pppEx() -> Int {
+        return 3
+    }
+}
