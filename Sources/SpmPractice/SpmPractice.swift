c6e32f769ee3d3054386d8eaf04f86ccf4cedaee
socar-taro <taro@socar.kr>
2024-05-07 10:14:50 +0900

    wwowhowhwo

--- Sources/SpmPractice/SpmPractice.swift
+++ Sources/SpmPractice/SpmPractice.swift
@@ -1,35 +1,11 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+open class A {
 
-public protocol PPP {
-    var x: Int { get set }
-}
-
-public extension PPP {
-    func pppEx() -> Int {
-        return 3
+    open func qqq() {
+        print("qqq")
     }
 }
 
-extension PPP {
-    internal func pppInternal() -> Int {
-        return 1
-    }
-    public func pppPub() -> Int {
-        return 2
-    }
-}
+public class B: A {
 
-public class CCC {
-    public var x = 3
-    public init() {}
-}
-
-extension CCC {
-    internal func qqq() -> Int {
-        return 6
-    }
-    public func pubpub() -> Int {
-        return 0
-    }
+    var x: Int = 3
 }
